b72c5f0a74bbb083cdb2423e569671ae6c006375
angelomaia <request.angelo@gmail.com>
2024-04-01 15:19:29 -0300

    task 2 corrected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ApplicationController
     def index
+    end
+
+    def movies
         @genders = Gender.all
+        @directors = Director.all
+        @movies = Movie.all
+    end
+
+    def genders
+        @genders = Gender.all
+        @movies = Movie.all
+    end
+
+    def directors
         @directors = Director.all
         @movies = Movie.all
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root        to: 'home#index'
+  get '/movies', to: 'home#movies'
+  get '/genders', to: 'home#genders'
+  get '/directors', to: 'home#directors'
   resources :movies, only: [:show, :new, :create, :edit, :update]
   resources :genders, only: [:show, :new, :create, :edit, :update]
   resources :directors, only: [:show, :new, :create, :edit, :update]
